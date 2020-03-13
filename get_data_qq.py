05934db5bba08970f241e48c802564bd496dd4da
wangbin_vendor@sensetime.com <wangbin_vendor@sensetime.com>
2020-03-13 13:15:16 +0800

    update

--- get_data_qq.py
+++ get_data_qq.py
@@ -42,7 +42,7 @@ def get_data(urlx, regx):
         logger.error(e)
         pass
     web_page = webdata.content.decode()
-    logger.info(web_page)
+    # logger.info(web_page)
     data = re_format.findall(webdata.content.decode())
     # logger.info(data)
     if data:
